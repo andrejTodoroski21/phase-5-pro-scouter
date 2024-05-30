0898d80f3163a36f16c98254d9815bf9a417becb
Andrej <andrejtodoroski@yahoo.com>
2024-05-30 19:10:26 -0400

    added started tables

--- server/models.py
+++ server/models.py
@@ -30,3 +30,4 @@ class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     time_uploaded = db.Column(db.DateTime, nullable=False, server_default=db.func.now())
+    
